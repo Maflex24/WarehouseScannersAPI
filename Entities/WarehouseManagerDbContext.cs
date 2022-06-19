8a89e5852b572b92d4942002c3b7f4515e4a50e9
Maflex24 <maflex24@gmail.com>
2022-06-19 18:46:13 +0200

    Added Employee/Permissions entities and configuration

---
+++ Entities/WarehouseManagerDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WarehouseManagerAPI.Entities
+{
+    public class WarehouseManagerDbContext : DbContext
+    {
+        public WarehouseManagerDbContext(DbContextOptions<WarehouseManagerDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+
+            
+        }
+    }
+}
